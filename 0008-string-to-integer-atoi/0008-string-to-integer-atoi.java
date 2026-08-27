854f72b283dcebc361a0af32cf1b59e9f6be69d9
Mohd Shaquib Ansari <118913864+shaquib132@users.noreply.github.com>
2026-08-27 12:10:44 +0530

    Time: 1 ms (100%), Space: 44 MB (23.01%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int myAtoi(String s) {
+
+        int i = 0;
+        int n = s.length();
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        int sign = 1;
+
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+
+        long number = 0;
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+
+            number = number * 10 + (s.charAt(i) - '0');
+
+            if (sign * number > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+
+            if (sign * number < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+
+            i++;
+        }
+
+        return (int) (sign * number);
+    }
+}
