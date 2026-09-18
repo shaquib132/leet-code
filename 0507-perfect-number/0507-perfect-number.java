848b673d95093ab33a0cead5d75930a37c63ea4d
Mohd Shaquib Ansari <118913864+shaquib132@users.noreply.github.com>
2026-09-18 18:14:05 +0530

    Time: 0 ms (100%), Space: 42 MB (62.5%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int n = num/2,sum = 0;
-        for(int i = 1; i<=n; i++){
-           if(num%i == 0){
-            sum +=i;
-           }
+        // int n = num/2,sum = 0;
+        // for(int i = 1; i<=n; i++){
+        //    if(num%i == 0){
+        //     sum +=i;
+        //    }
+        // }
+        // return num == sum;
+        if (num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336) {
+            return true;
+        } else {
+            return false;
         }
-        return num == sum;
     }
 }
